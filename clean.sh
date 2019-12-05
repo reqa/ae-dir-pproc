2e3f8e42a5c46841c8ca7de941af0ca44d444aa2
Michael Ströder <michael@stroeder.com>
2019-12-06 00:30:34 +0100

    python3 used in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python setup.py clean --all
+python3 setup.py clean --all
 rm -r MANIFEST .coverage dist/aedir_pproc* build/* *.egg-info .tox .eggs docs/.build/*
 rm aedir_pproc/*.py? aedir_pproc/pwd/*.py? tests/*.py? *.py?
 find -name __pycache__ | xargs -n1 -iname rm -r name
