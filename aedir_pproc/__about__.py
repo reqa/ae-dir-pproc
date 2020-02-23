3d8c1693d1ea06dc4b5ef9e767e5a49434f3585d
Michael Ströder <michael@stroeder.com>
2020-02-23 20:41:02 +0100

    bumped version to 0.6.0

--- aedir_pproc/__about__.py
+++ aedir_pproc/__about__.py
@@ -8,7 +8,7 @@ import collections
 VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro'))
 __version_info__ = VersionInfo(
     major=0,
-    minor=5,
+    minor=6,
     micro=0,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
