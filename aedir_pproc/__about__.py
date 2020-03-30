506e0640175d9c166ae54f94f15dbd9cb5d72696
Michael Ströder <michael@stroeder.com>
2020-03-30 13:13:45 +0200

    bumped version to 1.0.0

--- aedir_pproc/__about__.py
+++ aedir_pproc/__about__.py
@@ -7,9 +7,9 @@ import collections
 
 VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro'))
 __version_info__ = VersionInfo(
-    major=0,
-    minor=6,
-    micro=3,
+    major=1,
+    minor=0,
+    micro=0,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = u'Michael Stroeder'
