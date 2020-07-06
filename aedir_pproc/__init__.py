24545a9ec182af3e7b20c95c215cb994ce243f1d
Michael Ströder <michael@stroeder.com>
2020-07-06 19:02:32 +0200

    do not set env var LDAPRC

--- aedir_pproc/__init__.py
+++ aedir_pproc/__init__.py
@@ -4,11 +4,3 @@ Module package aedir_pproc
 """
 
 from .__about__ import __version__, __author__, __license__
-
-import os
-
-# set LDAPRC env var *before* importing ldap0
-os.environ['LDAPRC'] = '/opt/ae-dir/etc/ldap.conf'
-
-# from ldap0 package
-import ldap0
