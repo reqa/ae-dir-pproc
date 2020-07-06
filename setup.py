06820df697eb7cddb5beb4bb8a82a81bb51eeef2
Michael Ströder <michael@stroeder.com>
2020-07-06 18:29:05 +0200

    require aedir>=1.3.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     data_files=[],
     install_requires=[
         'setuptools',
-        'aedir>=0.8.0',
+        'aedir>=1.3.0',
         'mailutil>=0.4.0',
         'web.py>=0.40',
     ],
