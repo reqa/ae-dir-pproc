34f1a4b08cbc9b0a94bdabbd9ceafe8e96aaa8c5
Michael Ströder <michael@stroeder.com>
2021-04-13 22:11:24 +0200

    improved clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3 setup.py clean --all
-rm -rf MANIFEST .coverage dist/aedir_pproc* build/* *.egg-info .tox .eggs docs/.build/*
+rm -rf MANIFEST .coverage dist/aedir_pproc* build/* *.egg-info .tox .eggs docs/.build/* .mypy_cache
 rm -f aedir_pproc/*.py? aedir_pproc/pwd/*.py? tests/*.py? *.py?
 find -name __pycache__ | xargs -iname rm -r name
 rm -rf slapdtest-[0-9]*
